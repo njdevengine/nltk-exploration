224a8002779ee49fd8b2f1c858ef1fd90485de9f
njdevengine <newjerseydevelopment@gmail.com>
2019-04-02 15:02:42 -0400

    Update nltk-chapter-1.py

--- nltk-chapter-1.py
+++ nltk-chapter-1.py
@@ -21,3 +21,23 @@ import numpy
 #creates a graphical representation of where words appear, plots in a matplotlib chart.
 text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
 #http://www.nltk.org/images/inaugural.png
+
+sorted(set(text3))
+#returns all individual words and punctuation(tokens) sorted:
+# ['!', "'", '(', ')', ',', ',)', '.', '.)', ':', ';', ';)', '?', '?)',
+# 'A', 'Abel', 'Abelmizraim', 'Abidah', 'Abide', 'Abimael', 'Abimelech',
+# 'Abr', 'Abrah', 'Abraham', 'Abram', 'Accad', 'Achbor', 'Adah', ...]
+
+len(set(text3))
+#returns the number of unique (distinct words) word types / (punctuation) tokens
+
+len(set(text3)) / len(text3)
+#returns the lexical richness of the text, distinct words are just 6% here. Each word is used 16* on avg.
+
+#count how many times a word is used	
+text3.count("smote")
+# returns: 5
+#calc percentage of text a word is, a is 1 of this text (genesis)
+100 * text4.count('a') / len(text4)
+# returns: 1.4643016433938312
+
