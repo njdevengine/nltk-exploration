4964bcfbb8bfebdbb83b558a76349bc3d7ab3024
njdevengine <newjerseydevelopment@gmail.com>
2019-04-02 14:39:03 -0400

    Update nltk-chapter-1.py

--- nltk-chapter-1.py
+++ nltk-chapter-1.py
@@ -17,3 +17,8 @@ text2.common_contexts(["very", "always"])
 
 text2.concordance("unwilling")
 #has both:"I was very unwilling to enter" and "though always unwilling to join any"
+
+import matplotlib
+import numpy
+#creates a graphical representation of where words appear, plots in a matplotlib chart.
+text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
