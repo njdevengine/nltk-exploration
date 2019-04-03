518628fcd774838836aba10e758e6cadc56d6c35
njdevengine <newjerseydevelopment@gmail.com>
2019-04-03 10:25:36 -0400

    Update nltk-chapter-2.py

--- nltk-chapter-2.py
+++ nltk-chapter-2.py
@@ -1,4 +1,9 @@
 import nltk
 
 #import large body of  text from project gutenberg (books)
-nltk.corpus.gutenberg.fileids()
+#nltk.corpus.gutenberg.fileids()
+emma = nltk.corpus.gutenberg.words('austen-emma.txt')
+
+#convert to a text object, to perform nltk functions
+emma = nltk.Text(nltk.corpus.gutenberg.words('austen-emma.txt'))
+emma.concordance("surprize")
