1b6c19afa0b985e62503bf6e3bc50883872c27dd
njdevengine <newjerseydevelopment@gmail.com>
2019-04-03 12:16:14 -0400

    Update nltk-chapter-2.py

--- nltk-chapter-2.py
+++ nltk-chapter-2.py
@@ -6,4 +6,26 @@ emma = nltk.corpus.gutenberg.words('austen-emma.txt')
 
 #convert to a text object, to perform nltk functions
 emma = nltk.Text(nltk.corpus.gutenberg.words('austen-emma.txt'))
+#contextual use of word surprize:
 emma.concordance("surprize")
+
+from nltk.corpus import gutenberg
+gutenberg.fileids()
+# returns: ['austen-emma.txt', 'austen-persuasion.txt', 'austen-sense.txt', ...]
+
+for fileid in gutenberg.fileids():
+    num_chars = len(gutenberg.raw(fileid))
+    num_words = len(gutenberg.words(fileid))
+    num_sents = len(gutenberg.sents(fileid))
+    num_vocab = len(set(w.lower() for w in gutenberg.words(fileid)))
+    print(round(num_chars/num_words), round(num_words/num_sents), round(num_words/num_vocab), fileid)
+# avg word length, sentence length, and lexical diversity 
+# 5 25 26 austen-emma.txt
+# 5 26 17 austen-persuasion.txt
+# 5 28 22 austen-sense.txt
+# 4 34 79 bible-kjv.txt
+# 5 19 5 blake-poems.txt
+# 4 19 14 bryant-stories.txt
+# 4 18 12 burgess-busterbrown.txt
+# 4 20 13 carroll-alice.txt
+# 5 20 12 chesterton-ball.txt
